15570caeb8788688120255aa3efdeae3e6026b75
rohith12257997 <A00245384@mycambrian.ca>
2021-12-14 20:46:03 -0500

    first commit

---
+++ MvcMasterDetails/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MvcMasterDetails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMasterDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Post action for Save data to database
+        [HttpPost]
+        public JsonResult SaveOrder(OrderVM O)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    Order order = new Order { OrderNo = O.OrderNo, OrderDate = O.OrderDate, Description = O.Description };
+                    foreach (var i in O.OrderDetails)
+                    {                        
+                        order.OrderDetails.Add(i);
+                    }
+                    dc.Orders.Add(order);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            else
+            {
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status} };
+        }
+    }
+}
